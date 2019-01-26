eb33278dbefb30ccc0e3ff1524dfc99373512f38
Malavika Sreekumar <35482521+malavikasreekumar@users.noreply.github.com>
2019-01-26 11:21:02 +0530

    Add files via upload
    Swapping two nos

---
+++ swap.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 26 10:58:22 2019
+
+@author: Malavika  Sreekumar
+"""
+
+a=int(input("Enter No 1:"))
+b=int(input("Enter no 2:"))
+
+temp=a
+a=b
+b=temp
+
+print("The value of No1:{}".format(a))
+print("The value of No2:{}".format(b))
